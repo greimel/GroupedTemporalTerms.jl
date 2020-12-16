4bb29955250f2b6c85f2d3f42c3f7e965bc3e908
Fabian Greimel <fabgrei@gmail.com>
2020-12-16 18:01:41 +0100

    add test: use categorical grouping variable
    simplify test (only need three groups)
    
    
    fix test
    
    
    replace test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,36 +3,43 @@ pkg"add https://github.com/greimel/FixedEffectModels.jl#missings"
 
 using GroupedTemporalTerms: groupedlag
 using Test
-using DataFrames, Underscores
-using FixedEffectModels#: @formula, reg
+using DataFrames 
+using Underscores: @_
+using FixedEffectModels: @formula, reg
+using CategoricalArrays: categorical
 using ShiftedArrays: lag
-using StatsModels#: StatsModels, schema, apply_schema, modelcols
-
-
+using StatsModels: StatsModels, schema, apply_schema, modelcols, coef
 
 @testset "GroupedTemporalTerms.jl" begin
-    df = DataFrame(y = ones(10),
-                   x =   [1,   2  , 11,  12,  3,   13,  4,   5,   14,  15],
-                   grp = ["a", "a", "b", "b", "a", "b", "a", "a", "b", "b" ]
-                   )
-
-    f = @formula(y ~ x + grp + groupedlag(x, 1, grp))
+    # create fake data
+    df = let
+        group_ids = rand(1:3, 1000)
+        groups = ["a", "b", "c"][group_ids]
+        x = rand(1000)
+        
+        df = DataFrame(grp = groups, grp_id = group_ids, x = x)
+        @_ groupby(df, :grp) |> 
+            transform!(__, :x => lag => :x_lagged) |>
+            transform!(__, [:grp_id, :x_lagged] => ByRow((g, xl) -> π * g + xl) => :y)
+        
+        df[!, :y] = coalesce.(df.y, 0.0)
+        df
+    end
+    
+    # check that correct model matrix is created
+    f = @formula(y ~ grp + groupedlag(x, 1, grp))
     
     sch = schema(f, df)
     ff = apply_schema(f, sch)
     y, X = modelcols(ff, df)
     
     df_verify = DataFrame(X)
-    rename!(df_verify, [:x, :grp, :x_lagged])
-    df_verify[!,:grp] = copy(df.grp)
+    rename!(df_verify, StatsModels.coefnames(ff)[2])
     
-    @_ groupby(df_verify, :grp) |> 
-        transform!(__, :x => lag => :x_lagged_verify)
-        
-    @test isequal(df_verify.x_lagged, df_verify.x_lagged_verify)
-    
-        
+    @test isequal(df_verify."lag(x, 1)", df.x_lagged)
+
+    # test regression with patched version of FixedEffectModels.jl
     out = reg(df, f)
-    @test coef(out) ≈ [0, 1, 0, -1]
+    @test coef(out)[[1,end]] ≈ [π, 1]
 end
 
